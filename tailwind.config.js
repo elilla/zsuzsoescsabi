1f55c8438f4249a81e9e3490331ec33aad80f457
Lilla Enesei <enesei.lilla@gmail.com>
2022-03-16 18:38:11 +0100

    fix mobile layout and add dns

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
