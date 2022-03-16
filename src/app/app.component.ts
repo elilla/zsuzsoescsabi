68cfca6f13bbbf7845adf2e75789a6414b1595cc
Lilla Enesei <enesei.lilla@gmail.com>
2022-03-16 16:41:22 +0100

    add the landing page first look

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'zsuzsoescsabi';
+  days: number | undefined;
+  hours: number | undefined;
+  minutes: number | undefined;
+  seconds: number | undefined;
+  distance: number | undefined;
+
+  ngOnInit() {
+    const countDownDate = new Date("Jul 9, 2022 15:30:00").getTime();
+
+    setInterval(() => {
+      const now = new Date().getTime();
+      this.distance = countDownDate - now;
+      if (this.distance < 0) {
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+        return;
+      }
+      // Time calculations for days, hours, minutes and seconds
+      this.days = Math.floor(this.distance / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((this.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((this.distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((this.distance % (1000 * 60)) / 1000);
+    }, 1000);
+  }
 }
